a6d9d46044b9b85f26ae95ab3a5a5d4d9c78fca1
aymanali <ayman.fahmy@ieee-cusb.org>
2017-05-06 22:31:55 +0200

    Refactoring UnitTest project

---
+++ nResultUnitTest/NResultTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nResult_task.ViewModel;
+
+namespace nResultUnitTest
+{
+    [TestClass]
+    public class NResultTests
+    {
+        [TestMethod]
+        public void TestPagesCount()
+        {
+            MainViewModel CustomerVm = new MainViewModel();
+            int count = CustomerVm.PagesCount;
+            Assert.IsTrue(count == 0);
+        }
+
+        [TestMethod]
+        public void TestNavigation()
+        {
+            MainViewModel CustomerVm = new MainViewModel();
+            bool gotoFirst = CustomerVm.FirstEnabled;
+            Assert.IsTrue(gotoFirst == false);
+        }
+    }
+}
